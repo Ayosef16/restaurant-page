0889cd4e6fd818902dc475e2579ad7bbf3d42308
Ayosef <syrivera@uc.cl>
2023-05-29 14:23:01 -0400

    Add schedule section to home

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,5 +1,10 @@
 const createHomePage = () => {
     const restaurantContent = document.querySelector('.restaurant-content');
+    createAboutUsSection(restaurantContent);
+    createScheduleSection(restaurantContent);
+}
+
+function createAboutUsSection (name) {
     let aboutUs = document.createElement('div');
     let aboutUsHeader = document.createElement('div');
     let aboutUsContent = document.createElement('div');
@@ -9,10 +14,24 @@ const createHomePage = () => {
     and japanese.';
     aboutUsHeader.classList.add('about-us-header');
     aboutUsContent.classList.add('about-us-content');
+    aboutUs.classList.add('about-us');
     aboutUs.appendChild(aboutUsHeader);
     aboutUs.appendChild(aboutUsContent);
-    aboutUs.classList.add('about-us');
-    restaurantContent.appendChild(aboutUs);
+    name.appendChild(aboutUs);
+}
+
+function createScheduleSection (name) {
+    let schedule = document.createElement('div');
+    let scheduleHeader = document.createElement('div');
+    let scheduleContent = document.createElement('div');
+    scheduleHeader.textContent = 'Schedule';
+    scheduleContent.textContent = 'Monday - Friday: 8:00 - 20:00\r\nWeekend and Holidays: 8:00 - 22:00';
+    scheduleHeader.classList.add('schedule-header');
+    scheduleContent.classList.add('schedule-content');
+    schedule.classList.add('schedule');
+    schedule.appendChild(scheduleHeader);
+    schedule.appendChild(scheduleContent);
+    name.appendChild(schedule);
 }
 
 export default createHomePage;
